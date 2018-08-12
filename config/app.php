84e0cb7c1f08550736f54ceac398d55b4f1afb4d
Unknown <achraf.ghb@gmail.com>
2018-08-12 16:09:49 +0200

    Config Ebay

--- config/app.php
+++ config/app.php
@@ -151,6 +151,7 @@ return [
          * Package Service Providers...
          */
         Corcel\Laravel\CorcelServiceProvider::class,
+        Hkonnet\LaravelEbay\EbayServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -209,6 +210,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Ebay' => Hkonnet\LaravelEbay\Facade\Ebay::class,
 
     ],
 
