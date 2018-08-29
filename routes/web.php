7053cf1c027131184cddcc918a2ed59e90cde3ba
Unknown <achraf.ghb@gmail.com>
2018-08-29 23:32:19 +0200

    Routes Fixes : Middleware not integrated

--- routes/web.php
+++ routes/web.php
@@ -9,35 +9,34 @@ Auth::routes();
 |------------------------------------------------------------------------------------
 */
 
-Route::get('/', 'DashboardController@index')->name('dash');
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/', 'DashboardController@index')->name('dash');
 
-Route::resource('users', 'UserController');
+    Route::resource('users', 'UserController');
 
-Route::resource('products', 'ProductController');
+    Route::resource('products', 'ProductController');
 
-Route::prefix('feedback')->group(function(){
-    Route::get('/','FeedBackController')->name('feed');
-    Route::get('eby','EbayController@index')->name('ebay');
-    Route::get('eby/product/{id}','EbayController@product');
-    Route::post('eby/msg','EbayController@answer')->name('respondebay');
-    Route::post('eby/fbmsg','EbayController@answerfb')->name('respondfbebay');
-    Route::get('amz','AmazonController@index')->name('amazon');
-    Route::get('fbm','FacebookController@index')->name('facebook');
-    Route::get('gmt','GumtreeController@index')->name('gumtree');
-    Route::get('gpl','GooglePlusController@index')->name('google');
+    Route::prefix('feedback')->group(function(){
+        Route::get('/','FeedBackController')->name('feed');
+        Route::get('eby','EbayController@index')->name('ebay');
+        Route::get('eby/product/{id}','EbayController@product');
+        Route::post('eby/msg','EbayController@answer')->name('respondebay');
+        Route::post('eby/fbmsg','EbayController@answerfb')->name('respondfbebay');
+        Route::get('amz','AmazonController@index')->name('amazon');
+        Route::get('fbm','FacebookController@index')->name('facebook');
+        Route::get('gmt','GumtreeController@index')->name('gumtree');
+        Route::get('gpl','GooglePlusController@index')->name('google');
+    });
+
+    Route::get('/test',function(){
+        return view('admin.feedback.test');
+    });
+    Route::get('/test2',function(){
+        return view('admin.feedback.test2');
+    });
+    Route::get('/test4',function(){
+        return view('admin.feedback.test4');
+    });
 });
 
-Route::get('/test',function(){
-    return view('admin.feedback.test');
-});
-Route::get('/test2',function(){
-    return view('admin.feedback.test2');
-});
-Route::get('/test4',function(){
-    return view('admin.feedback.test4');
-});
-
-Route::get('/home', function () {
-    return view('home');
-});
 
